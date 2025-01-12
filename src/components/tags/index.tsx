58a6f4f0284b6b9fcc41c4cbb5f8940f8c1613ef
praveenvuddagiri <vuddagiripraveen@gmail.com>
2025-01-12 13:28:52 +0530

    added style type to tags

--- src/components/tags/index.tsx
+++ src/components/tags/index.tsx
@@ -2,228 +2,229 @@
 
 import styled from "@emotion/styled";
 import { Mess } from "../../core";
+import { Styles } from "../../core/types/mess";
 
 export const Button = styled.button(
-  ({ styles, customeclasses }: { styles: any; customeclasses?: string }) =>
+  ({ styles, customeclasses }: { styles: Styles; customeclasses?: string }) =>
     Mess(styles, customeclasses)
 );
 export const Text = styled.p(
-  ({ styles, customeclasses }: { styles: any; customeclasses?: string }) =>
+  ({ styles, customeclasses }: { styles: Styles; customeclasses?: string }) =>
     Mess(styles, customeclasses)
 );
 export const Image = styled.img(
-  ({ styles, customeclasses }: { styles: any; customeclasses?: string }) =>
+  ({ styles, customeclasses }: { styles: Styles; customeclasses?: string }) =>
     Mess(styles, customeclasses)
 );
 export const Box = styled.div(
-  ({ styles, customeclasses }: { styles: any; customeclasses?: string }) =>
+  ({ styles, customeclasses }: { styles: Styles; customeclasses?: string }) =>
     Mess(styles, customeclasses)
 );
 export const Input = styled.input(
-  ({ styles, customeclasses }: { styles: any; customeclasses?: string }) =>
+  ({ styles, customeclasses }: { styles: Styles; customeclasses?: string }) =>
     Mess(styles, customeclasses)
 );
 export const Label = styled.label(
-  ({ styles, customeclasses }: { styles: any; customeclasses?: string }) =>
+  ({ styles, customeclasses }: { styles: Styles; customeclasses?: string }) =>
     Mess(styles, customeclasses)
 );
 export const Ul = styled.ul(
-  ({ styles, customeclasses }: { styles: any; customeclasses?: string }) =>
+  ({ styles, customeclasses }: { styles: Styles; customeclasses?: string }) =>
     Mess(styles, customeclasses)
 );
 export const Li = styled.li(
-  ({ styles, customeclasses }: { styles: any; customeclasses?: string }) =>
+  ({ styles, customeclasses }: { styles: Styles; customeclasses?: string }) =>
     Mess(styles, customeclasses)
 );
 export const Footer = styled.footer(
-  ({ styles, customeclasses }: { styles: any; customeclasses?: string }) =>
+  ({ styles, customeclasses }: { styles: Styles; customeclasses?: string }) =>
     Mess(styles, customeclasses)
 );
 export const Form = styled.form(
-  ({ styles, customeclasses }: { styles: any; customeclasses?: string }) =>
+  ({ styles, customeclasses }: { styles: Styles; customeclasses?: string }) =>
     Mess(styles, customeclasses)
 );
 export const Span = styled.span(
-  ({ styles, customeclasses }: { styles: any; customeclasses?: string }) =>
+  ({ styles, customeclasses }: { styles: Styles; customeclasses?: string }) =>
     Mess(styles, customeclasses)
 );
 export const A = styled.a(
-  ({ styles, customeclasses }: { styles: any; customeclasses?: string }) =>
+  ({ styles, customeclasses }: { styles: Styles; customeclasses?: string }) =>
     Mess(styles, customeclasses)
 );
 export const Select = styled.select(
-  ({ styles, customeclasses }: { styles: any; customeclasses?: string }) =>
+  ({ styles, customeclasses }: { styles: Styles; customeclasses?: string }) =>
     Mess(styles, customeclasses)
 );
 export const Option = styled.option(
-  ({ styles, customeclasses }: { styles: any; customeclasses?: string }) =>
+  ({ styles, customeclasses }: { styles: Styles; customeclasses?: string }) =>
     Mess(styles, customeclasses)
 );
 export const Header = styled.header(
-  ({ styles, customeclasses }: { styles: any; customeclasses?: string }) =>
+  ({ styles, customeclasses }: { styles: Styles; customeclasses?: string }) =>
     Mess(styles, customeclasses)
 );
 export const Main = styled.main(
-  ({ styles, customeclasses }: { styles: any; customeclasses?: string }) =>
+  ({ styles, customeclasses }: { styles: Styles; customeclasses?: string }) =>
     Mess(styles, customeclasses)
 );
 export const Article = styled.article(
-  ({ styles, customeclasses }: { styles: any; customeclasses?: string }) =>
+  ({ styles, customeclasses }: { styles: Styles; customeclasses?: string }) =>
     Mess(styles, customeclasses)
 );
 export const Section = styled.section(
-  ({ styles, customeclasses }: { styles: any; customeclasses?: string }) =>
+  ({ styles, customeclasses }: { styles: Styles; customeclasses?: string }) =>
     Mess(styles, customeclasses)
 );
 export const Aside = styled.aside(
-  ({ styles, customeclasses }: { styles: any; customeclasses?: string }) =>
+  ({ styles, customeclasses }: { styles: Styles; customeclasses?: string }) =>
     Mess(styles, customeclasses)
 );
 export const Nav = styled.nav(
-  ({ styles, customeclasses }: { styles: any; customeclasses?: string }) =>
+  ({ styles, customeclasses }: { styles: Styles; customeclasses?: string }) =>
     Mess(styles, customeclasses)
 );
 export const H1 = styled.h1(
-  ({ styles, customeclasses }: { styles: any; customeclasses?: string }) =>
+  ({ styles, customeclasses }: { styles: Styles; customeclasses?: string }) =>
     Mess(styles, customeclasses)
 );
 export const H2 = styled.h2(
-  ({ styles, customeclasses }: { styles: any; customeclasses?: string }) =>
+  ({ styles, customeclasses }: { styles: Styles; customeclasses?: string }) =>
     Mess(styles, customeclasses)
 );
 export const H3 = styled.h3(
-  ({ styles, customeclasses }: { styles: any; customeclasses?: string }) =>
+  ({ styles, customeclasses }: { styles: Styles; customeclasses?: string }) =>
     Mess(styles, customeclasses)
 );
 export const H4 = styled.h4(
-  ({ styles, customeclasses }: { styles: any; customeclasses?: string }) =>
+  ({ styles, customeclasses }: { styles: Styles; customeclasses?: string }) =>
     Mess(styles, customeclasses)
 );
 export const H5 = styled.h5(
-  ({ styles, customeclasses }: { styles: any; customeclasses?: string }) =>
+  ({ styles, customeclasses }: { styles: Styles; customeclasses?: string }) =>
     Mess(styles, customeclasses)
 );
 export const H6 = styled.h6(
-  ({ styles, customeclasses }: { styles: any; customeclasses?: string }) =>
+  ({ styles, customeclasses }: { styles: Styles; customeclasses?: string }) =>
     Mess(styles, customeclasses)
 );
 export const Table = styled.table(
-  ({ styles, customeclasses }: { styles: any; customeclasses?: string }) =>
+  ({ styles, customeclasses }: { styles: Styles; customeclasses?: string }) =>
     Mess(styles, customeclasses)
 );
 export const Thead = styled.thead(
-  ({ styles, customeclasses }: { styles: any; customeclasses?: string }) =>
+  ({ styles, customeclasses }: { styles: Styles; customeclasses?: string }) =>
     Mess(styles, customeclasses)
 );
 export const Tbody = styled.tbody(
-  ({ styles, customeclasses }: { styles: any; customeclasses?: string }) =>
+  ({ styles, customeclasses }: { styles: Styles; customeclasses?: string }) =>
     Mess(styles, customeclasses)
 );
 export const Tr = styled.tr(
-  ({ styles, customeclasses }: { styles: any; customeclasses?: string }) =>
+  ({ styles, customeclasses }: { styles: Styles; customeclasses?: string }) =>
     Mess(styles, customeclasses)
 );
 export const Th = styled.th(
-  ({ styles, customeclasses }: { styles: any; customeclasses?: string }) =>
+  ({ styles, customeclasses }: { styles: Styles; customeclasses?: string }) =>
     Mess(styles, customeclasses)
 );
 export const Td = styled.td(
-  ({ styles, customeclasses }: { styles: any; customeclasses?: string }) =>
+  ({ styles, customeclasses }: { styles: Styles; customeclasses?: string }) =>
     Mess(styles, customeclasses)
 );
 export const Pre = styled.pre(
-  ({ styles, customeclasses }: { styles: any; customeclasses?: string }) =>
+  ({ styles, customeclasses }: { styles: Styles; customeclasses?: string }) =>
     Mess(styles, customeclasses)
 );
 export const Code = styled.code(
-  ({ styles, customeclasses }: { styles: any; customeclasses?: string }) =>
+  ({ styles, customeclasses }: { styles: Styles; customeclasses?: string }) =>
     Mess(styles, customeclasses)
 );
 export const Blockquote = styled.blockquote(
-  ({ styles, customeclasses }: { styles: any; customeclasses?: string }) =>
+  ({ styles, customeclasses }: { styles: Styles; customeclasses?: string }) =>
     Mess(styles, customeclasses)
 );
 export const Hr = styled.hr(
-  ({ styles, customeclasses }: { styles: any; customeclasses?: string }) =>
+  ({ styles, customeclasses }: { styles: Styles; customeclasses?: string }) =>
     Mess(styles, customeclasses)
 );
 export const Br = styled.br(
-  ({ styles, customeclasses }: { styles: any; customeclasses?: string }) =>
+  ({ styles, customeclasses }: { styles: Styles; customeclasses?: string }) =>
     Mess(styles, customeclasses)
 );
 export const Progress = styled.progress(
-  ({ styles, customeclasses }: { styles: any; customeclasses?: string }) =>
+  ({ styles, customeclasses }: { styles: Styles; customeclasses?: string }) =>
     Mess(styles, customeclasses)
 );
 export const Iframe = styled.iframe(
-  ({ styles, customeclasses }: { styles: any; customeclasses?: string }) =>
+  ({ styles, customeclasses }: { styles: Styles; customeclasses?: string }) =>
     Mess(styles, customeclasses)
 );
 export const Canvas = styled.canvas(
-  ({ styles, customeclasses }: { styles: any; customeclasses?: string }) =>
+  ({ styles, customeclasses }: { styles: Styles; customeclasses?: string }) =>
     Mess(styles, customeclasses)
 );
 export const Video = styled.video(
-  ({ styles, customeclasses }: { styles: any; customeclasses?: string }) =>
+  ({ styles, customeclasses }: { styles: Styles; customeclasses?: string }) =>
     Mess(styles, customeclasses)
 );
 export const Audio = styled.audio(
-  ({ styles, customeclasses }: { styles: any; customeclasses?: string }) =>
+  ({ styles, customeclasses }: { styles: Styles; customeclasses?: string }) =>
     Mess(styles, customeclasses)
 );
 export const Figure = styled.figure(
-  ({ styles, customeclasses }: { styles: any; customeclasses?: string }) =>
+  ({ styles, customeclasses }: { styles: Styles; customeclasses?: string }) =>
     Mess(styles, customeclasses)
 );
 export const Figcaption = styled.figcaption(
-  ({ styles, customeclasses }: { styles: any; customeclasses?: string }) =>
+  ({ styles, customeclasses }: { styles: Styles; customeclasses?: string }) =>
     Mess(styles, customeclasses)
 );
 export const Details = styled.details(
-  ({ styles, customeclasses }: { styles: any; customeclasses?: string }) =>
+  ({ styles, customeclasses }: { styles: Styles; customeclasses?: string }) =>
     Mess(styles, customeclasses)
 );
 export const Summary = styled.summary(
-  ({ styles, customeclasses }: { styles: any; customeclasses?: string }) =>
+  ({ styles, customeclasses }: { styles: Styles; customeclasses?: string }) =>
     Mess(styles, customeclasses)
 );
 export const Mark = styled.mark(
-  ({ styles, customeclasses }: { styles: any; customeclasses?: string }) =>
+  ({ styles, customeclasses }: { styles: Styles; customeclasses?: string }) =>
     Mess(styles, customeclasses)
 );
 export const Time = styled.time(
-  ({ styles, customeclasses }: { styles: any; customeclasses?: string }) =>
+  ({ styles, customeclasses }: { styles: Styles; customeclasses?: string }) =>
     Mess(styles, customeclasses)
 );
 export const Meter = styled.meter(
-  ({ styles, customeclasses }: { styles: any; customeclasses?: string }) =>
+  ({ styles, customeclasses }: { styles: Styles; customeclasses?: string }) =>
     Mess(styles, customeclasses)
 );
 export const B = styled.b(
-  ({ styles, customeclasses }: { styles: any; customeclasses?: string }) =>
+  ({ styles, customeclasses }: { styles: Styles; customeclasses?: string }) =>
     Mess(styles, customeclasses)
 );
 export const I = styled.i(
-  ({ styles, customeclasses }: { styles: any; customeclasses?: string }) =>
+  ({ styles, customeclasses }: { styles: Styles; customeclasses?: string }) =>
     Mess(styles, customeclasses)
 );
 export const Small = styled.small(
-  ({ styles, customeclasses }: { styles: any; customeclasses?: string }) =>
+  ({ styles, customeclasses }: { styles: Styles; customeclasses?: string }) =>
     Mess(styles, customeclasses)
 );
 export const Strong = styled.strong(
-  ({ styles, customeclasses }: { styles: any; customeclasses?: string }) =>
+  ({ styles, customeclasses }: { styles: Styles; customeclasses?: string }) =>
     Mess(styles, customeclasses)
 );
 export const Abbr = styled.abbr(
-  ({ styles, customeclasses }: { styles: any; customeclasses?: string }) =>
+  ({ styles, customeclasses }: { styles: Styles; customeclasses?: string }) =>
     Mess(styles, customeclasses)
 );
 export const Sub = styled.sub(
-  ({ styles, customeclasses }: { styles: any; customeclasses?: string }) =>
+  ({ styles, customeclasses }: { styles: Styles; customeclasses?: string }) =>
     Mess(styles, customeclasses)
 );
 export const Sup = styled.sup(
-  ({ styles, customeclasses }: { styles: any; customeclasses?: string }) =>
+  ({ styles, customeclasses }: { styles: Styles; customeclasses?: string }) =>
     Mess(styles, customeclasses)
 );
